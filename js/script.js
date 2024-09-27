0636b0bf91ff94bee74383f82711ab07d6afa04e
tsasisa <rochalineweyln@gmail.com>
2024-09-27 21:14:55 +0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+/*menu toggle*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
+/*scroll effect 1*/
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec=> {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top>= offset && top < offset + height){
+            navLinks.forEach(links=> {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+};
+
+
+/*scroll effect*/
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+
+ScrollReveal().reveal('.home-content, .heading', {origin: 'top'});
+ScrollReveal().reveal('.home-img, .docum-container, .portofolio-box', {origin: 'top'});
+ScrollReveal().reveal('.home-content h1', {origin: 'left'});
+
+/*typed effect*/
+const typed = new Typed('.multiple-text',{
+    strings: ['Software Engineer','Back-End Developer','Front-End Developer','Full-Stack Developer', 'UI/UX Designer'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+
+});
+
